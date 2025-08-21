281ce63098ea7cca88b9bd9d3c1189cba06d7fd0
amrhosny11 <amr.hosny24.8.12@gmail.com>
2025-08-21 22:24:48 +0300

    update:reload after win and lose

--- main.js
+++ main.js
@@ -9,50 +9,24 @@ let lettersContainer = document.querySelector(".letters");
 
 // Generate Letters
 lettersArray.forEach(letter => {
-
-  // Create Span
   let span = document.createElement("span");
-
-  // Create Letter Text Node
   let theLetter = document.createTextNode(letter);
-
-  // Append The Letter To Span
   span.appendChild(theLetter);
-
-  // Add Class On Span
   span.className = 'letter-box';
-
-  // Append Span To The Letters Container
   lettersContainer.appendChild(span);
-
 });
 
 // Object Of Words + Categories
 const words = {
-//   programming: ["php", "javascript", "go", "scala", "fortran", "r", "mysql", "python"],s
-//   movies: ["Prestige", "Inception", "Parasite", "Interstellar", "Whiplash", "Memento", "Coco", "Up"],
-//   people: ["Albert Einstein", "Hitchcock", "Alexander", "Cleopatra", "Mahatma Ghandi"],
-//   countries: ["Syria", "Palestine", "Yemen", "Egypt", "Bahrain", "Qatar"],
   names:['amr','tamer',"hala","hosny","laila","yehia","younis","mohamed","mariem","ehsan","hana","sara","hayam","nelly"]
 }
 
 // Get Random Property
-
 let allKeys = Object.keys(words);
-
-// Random Number Depend On Keys Length
 let randomPropNumber = Math.floor(Math.random() * allKeys.length);
-
-// Category
 let randomPropName = allKeys[randomPropNumber];
-
-// Category Words
 let randomPropValue = words[randomPropName];
-
-// Random Number Depend On Words
 let randomValueNumber = Math.floor(Math.random() * randomPropValue.length);
-
-// The Chosen Word
 let randomValueValue = randomPropValue[randomValueNumber];
 
 // Set Category Info
@@ -66,21 +40,11 @@ let lettersAndSpace = Array.from(randomValueValue);
 
 // Create Spans Depened On Word
 lettersAndSpace.forEach(letter => {
-
-  // Create Empty Span
   let emptySpan = document.createElement("span");
-
-  // If Letter Is Space
   if (letter === ' ') {
-
-    // Add Class To The Span
     emptySpan.className = 'with-space';
-
   }
-
-  // Append Span To The Letters Guess Container
   lettersGuessContainer.appendChild(emptySpan);
-
 });
 
 // Select Guess Spans
@@ -94,92 +58,73 @@ let theDraw = document.querySelector(".hangman-draw");
 
 // Handle Clicking On Letters
 document.addEventListener("click", (e) => {
-
-  // Set The Choose Status
   let theStatus = false;
 
   if (e.target.className === 'letter-box') {
-
     e.target.classList.add("clicked");
 
-    // Get Clicked Letter
     let theClickedLetter = e.target.innerHTML.toLowerCase();
-
-    // The Chosen Word
     let theChosenWord = Array.from(randomValueValue.toLowerCase());
 
     theChosenWord.forEach((wordLetter, WordIndex) => {
-
-      // If The Clicked Letter Equal To One Of The Chosen Word Letter
       if (theClickedLetter == wordLetter) {
-
-        // Set Status To Correct
         theStatus = true;
-
-        // Loop On All Guess Spans
         guessSpans.forEach((span, spanIndex) => {
-
           if (WordIndex === spanIndex) {
-
             span.innerHTML = theClickedLetter;
-
           }
-
         });
-
       }
-
     });
 
-    // Outside Loop
+    // ✅ Check if the word is fully guessed
+    let allFilled = true;
+    guessSpans.forEach(span => {
+      if (span.innerHTML === "") {
+        allFilled = false;
+      }
+    });
 
-    // If Letter Is Wrong
+    if (allFilled) {
+      winGame(); // call win popup
+      setTimeout(() => {
+        location.reload();
+      }, 1000);
+    }
+
+    // If wrong
     if (theStatus !== true) {
-
-      // Increase The Wrong Attempts
       wrongAttempts++;
-
-      // Add Class Wrong On The Draw Element
       theDraw.classList.add(`wrong-${wrongAttempts}`);
-
-      // Play Fail Sound
       document.getElementById("fail").play();
 
       if (wrongAttempts === 8) {
-
         endGame();
-
         lettersContainer.classList.add("finished");
-
+              setTimeout(() => {
+        location.reload();
+      }, 4000);
       }
-
     } else {
-
-      // Play Success Sound
       document.getElementById("success").play();
-
     }
-
   }
-
 });
 
 // End Game Function
 function endGame() {
-
-  // Create Popup Div
   let div = document.createElement("div");
-
-  // Create Text
   let divText = document.createTextNode(`Game Over, The Word Is ${randomValueValue}`);
-
-  // Append Text To Div
   div.appendChild(divText);
-
-  // Add Class On Div
   div.className = 'popup';
-
-  // Append To The Body
   document.body.appendChild(div);
+}
 
+// ✅ Win Game Function
+function winGame() {
+  let div = document.createElement("div");
+  let divText = document.createTextNode(`🎉 You Win! The Word Is ${randomValueValue}`);
+  div.appendChild(divText);
+  div.className = 'popup';
+  document.body.appendChild(div);
 }
